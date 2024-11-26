d98644d69182843d4ce0857b8290f0b3bdc4f40f
Johan <147413295+jkrangh@users.noreply.github.com>
2024-11-26 13:02:18 +0100

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace WpfHobbyList
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
